a49ed776879e031c96e76a87babc0c99ff80d87e
quitam <tamphamasd@gmail.com>
2024-02-22 15:39:58 +0700

    update: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(5001);
+  const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Notes API')
+    .setDescription('The notes API description')
+    .setVersion('1.0')
+    .addTag('notes')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(5001)
 }
-bootstrap();
+bootstrap()
